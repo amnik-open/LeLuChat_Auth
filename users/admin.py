4fa274f991e98965bd1e3b71b608edffa9e6e1b6
Amir Nikpour <amnik.free@gmail.com>
2023-11-28 19:13:27 +0330

    users: add models and admin for custom LeluUser

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,39 @@
-# pylint: skip-file
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin
+from users.models import LeluUser
 
-# Register your models here.
+
+class LeluUserAdmin(UserAdmin):
+    fieldsets = (
+    (None, {"fields": ("email", "password", "name")}),
+    (_("Personal info"), {"fields": ("first_name",
+    "last_name")}),
+    (
+        _("Permissions"),
+        {
+            "fields": (
+            "is_active",
+            "is_staff",
+            "is_superuser",
+            "groups",
+            "user_permissions",
+            )
+        },
+    ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("email", "name", "first_name", "last_name", "is_staff")
+    search_fields = ("email", "name", "first_name", "last_name")
+    ordering = ("email",)
+
+admin.site.register(LeluUser, LeluUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,4 +1,36 @@
-# pylint: skip-file
+from django.utils.translation import gettext_lazy as _
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
 
-# Create your models here.
+
+class LeluUserManager(UserManager):
+    def _create_user(self, email, password, name, **extra_fields):
+        if not email:
+            raise ValueError("Email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_user(self, email, name, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, name, password, **extra_fields)
+    def create_superuser(self, email, name, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self._create_user(email, name, password, **extra_fields)
+
+class LeluUser(AbstractUser):
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+    name = models.CharField(max_length=200)
+    objects = LeluUserManager()
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    def __str__(self):
+        return self.email
